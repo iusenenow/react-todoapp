e97650a122f3da1b61f62d247ab9cbf65c604544
Wei <iusenenow@gmail.com>
2020-01-23 12:49:40 +1100

    Updates

---
+++ precache-manifest.14c2fb4eba8e7954aa19612446facf52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b448f4e63ea460f36eadd88f10f1591",
+    "url": "/react-todoapp/index.html"
+  },
+  {
+    "revision": "a9d45caacaab8b9e879f",
+    "url": "/react-todoapp/static/css/main.32aac062.chunk.css"
+  },
+  {
+    "revision": "f405d0c5c6da3fd67f2c",
+    "url": "/react-todoapp/static/js/2.509e9b3c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todoapp/static/js/2.509e9b3c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9d45caacaab8b9e879f",
+    "url": "/react-todoapp/static/js/main.6b1547d6.chunk.js"
+  },
+  {
+    "revision": "d0364999d3912dc92210",
+    "url": "/react-todoapp/static/js/runtime-main.2c5075a7.js"
+  }
+]);
